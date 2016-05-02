beeb50d991f279ab79d528745e4c026e972adc52
Jeff Seymour <seym1585@gmail.com>
2016-05-01 17:47:19 -0700

    tweak styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,15 +18,16 @@ import {Keg} from './keg.model';
 })
 export class AppComponent {
   public kegs: Keg[];
+  /*For Kegs[]: Name, Brewery, Price, ABV%, ID*/
   constructor(){
     this.kegs = [
-      new Keg("Workhorse IPA", "Laurelwood", 5, 7.5, 0),
-      new Keg("Mirror Pond", "Deschutes", 4, 6, 1),
-      new Keg("Crush Cucumber Sour", "10 Barrel", 6.5, 5, 2),
-      new Keg("Workhorse IPA", "Laurelwood", 5, 7.5, 3),
-      new Keg("Mirror Pond", "Deschutes", 4, 6, 4),
-      new Keg("Crush Cucumber Sour", "10 Barrel", 6.5, 5, 5),
-      new Keg("Workhorse IPA", "Laurelwood", 5, 7.5, 6),
+      new Keg("Mirror Pond", "Deschutes", 4, 6, 0),
+      new Keg("Crush Cucumber Sour", "10 Barrel", 6.5, 5, 1),
+      new Keg("Workhorse IPA", "Laurelwood", 5, 7.5, 2),
+      new Keg("Mirror Pond", "Deschutes", 4, 6, 3),
+      new Keg("Black-Briar Mead", "Black-Briar Meadery", 10, 12.5, 4),
+      new Keg("Skooma", "Elsweyr", 5, 11, 5),
+      new Keg("Crush Cucumber Sour", "10 Barrel", 6.5, 5, 6),
       new Keg("Mirror Pond", "Deschutes", 4, 6, 7),
       new Keg("Crush Cucumber Sour", "10 Barrel", 6.5, 5, 8),
     ];
--- app/keg.component.ts
+++ app/keg.component.ts
@@ -19,8 +19,8 @@ import { EditKegDetailsComponent } from './edit-keg-details.component';
         </div>
 
           <p class="pintInfo {{ lowKegClass }} ">{{ "Pints: " + keg.pints  }}</p>
-
           <button class="buy-btn" (click)="buyPint(keg) ">Buy a pint!</button>
+
           <button class="edit-btn"(click)="editInfo(keg)">
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
             	 width="100%" height="100%" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
